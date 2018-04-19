866cab0c1c31eeb7aed9cc88d61f47894b8c014a
Felipe Sena <felipe.sena@mwmicroware.com.br>
2018-04-19 15:44:21 -0300

    add user, stock association, fix bug and codes and plus views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :user_stocks, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/index'
   root 'welcome#index'
+  get 'welcome/index'
 
-  get 'my_porfolio', to: 'users#my_portfolio'
+  get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  resources :user_stocks, only: [:create]
 
 end
