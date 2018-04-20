cc50671aa133585e70105c9cf93dadd61e1ed043
Felipe Sena <felipe.sena@mwmicroware.com.br>
2018-04-20 15:50:51 -0300

    add friends routes, models and controllers for basic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations"}
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
 
   #resources :user_stocks, except: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -12,5 +14,6 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
+
 
 end
